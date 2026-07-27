8df9548e3a15ddd89f786c4c89e95b01926802f2
PatrickC995 <campbellpatrick2004@gmail.com>
2026-07-27 11:23:00 +0100

    added state to text area

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import "./css/App.css";
 
 function App() {
+  const [text, setText] = useState("");
+
   return (
     <main>
       <h1>Text-to-Speech</h1>
 
-      <textarea id="textInput" placeholder="Enter text here..." rows={8} />
+      <textarea
+        id="textInput"
+        placeholder="Enter text here..."
+        rows={8}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      />
 
       <br />
 
