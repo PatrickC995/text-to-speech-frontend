cbdca85cc432d63608a24a24f675bd0c83aaa72a
PatrickC995 <campbellpatrick2004@gmail.com>
2026-08-05 21:33:15 +0100

    updated to revieve audioblob

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,18 @@ import "./css/App.css";
 function App() {
   const [text, setText] = useState("");
 
-  function handleGenerate() {
-    console.log(text);
+  async function handleGenerate() {
+    const response = await fetch("http://localhost:8080/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: text,
+      }),
+    });
+    const audioBlob = await response.blob();
+    console.log(audioBlob);
   }
 
   return (
